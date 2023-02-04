d41deb9415e18f627338b2829a23d75580645eb9
yusuffikri <yusuffiks99@gmail.com>
2023-02-05 06:21:53 +0800

    tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gray-20": "#F8F4EB",
+        "gray-50": "#EFE6E6",
+        "gray-100": "#DFCCCC",
+        "gray-100": "#5E0000",
+        "primary-100": "#FFE1E0",
+        "primary-300": "#FFA6A3",
+        "primary-500": "#FF6B66",
+        "secondary-400": "#FFCD58",
+        "secondary-500": "#FFC132",
+      }
+    },
   },
   plugins: [],
 }
