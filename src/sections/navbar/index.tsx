143f90f2f035a1c77b87a7a0ab3751120b326cbd
yusuffikri <yusuffiks99@gmail.com>
2023-02-05 07:58:21 +0800

    navbar update

--- src/sections/navbar/index.tsx
+++ src/sections/navbar/index.tsx
@@ -8,8 +8,28 @@ const Navbar = (props: Props) => {
     const flexBetween = "flex items-center justify-between";
 
   return <nav>
-    <div className={`${flexBetween} fixe top-0 z-30 w-full py-6`}>
+    <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+            <div className={`${flexBetween} w-full gap-16`}>
+                {/* Left Side */}
+                <img src={Logo} />
 
+                {/* Right Side */}
+                <div className={`${flexBetween} w-full`}>
+                    <div className={`${flexBetween} gap-8 text-sm`} >
+                        <p>Home</p>
+                        <p>Benefits</p>
+                        <p>Our Classes</p>
+                        <p>Contact Us</p>
+                    </div>
+                    <div>
+                        <p>Sign In</p>
+                        <button>Become a Member</button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   </nav>
 
