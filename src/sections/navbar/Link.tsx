b4c80f739fd5fbfe00dd03954cabf576f52c9fc8
yusuffikri <yusuffiks99@gmail.com>
2023-02-06 00:10:54 +0800

    navbar update

--- src/sections/navbar/Link.tsx
+++ src/sections/navbar/Link.tsx
@@ -2,17 +2,24 @@ import AnchorLink from "react-anchor-link-smooth-scroll"
 
 type Props = {
     page: string;
+    selectedPage: string;
+    setSelectedPage: (value: string) => void;
+
 }
 
 const Link = ({
     page,
+    selectedPage,
+    setSelectedPage,
 }: Props) => {
   const lowerCasePage = page.toLowerCase().replace(/ /g, "")
 
   return <AnchorLink
-  className={`#${selectedPage === lowerCasePage}`}
+  className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
+    transition-500 duration-500 hover:text-primary-300
+  `}
   href={`#${lowerCasePage}`}
-  onClick={}
+  onClick={() => setSelectedPage(lowerCasePage) }
   >
     {page}
   </AnchorLink>
