5935eb5c72e5404d4fd7b4702a5b799e3c390d94
yusuffikri <yusuffiks99@gmail.com>
2023-02-06 15:13:17 +0800

    update navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Navbar from '@/sections/navbar'
 
-function App(){
+function App({}){
   return <div className="app bg-gray-20">
   <Navbar/>
   </div>
--- src/sections/navbar/index.tsx
+++ src/sections/navbar/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid"
 import Logo from "@/assets/Logo.png"
+import Link from "./Link"
 
 type Props = {
     selectedPage: string;
@@ -16,7 +17,7 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
             <div className={`${flexBetween} w-full gap-16`}>
                 {/* Left Side */}
                 <img src={Logo} />
-
+ 
                 {/* Right Side */}
                 <div className={`${flexBetween} w-full`}>
                     <div className={`${flexBetween} gap-8 text-sm`} >
