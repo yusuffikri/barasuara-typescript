9ab04440628e7ac96e484f0c9f765b5373a17d81
yusuffikri <yusuffiks99@gmail.com>
2023-02-08 23:31:05 +0800

    navbar update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import Navbar from '@/sections/navbar'
 import { useState } from 'react';
 
+enum SelectedPage {
+  Home = "home",
+  Benefits ="benefits",
+  Classes = "classes",
+  Contacts = "contacs"
+}
+
 function App({}){
 
-  const [selectedPage,setSelectedPage] = useState<string>('home');
+  const [selectedPage,setSelectedPage] = useState<string>(SelectedPage.Home);
 
 
   return <div className="app bg-gray-20">
