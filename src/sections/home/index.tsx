893ab3cf7b2d6033a10f1c1ca844ba21e82a77d2
yusuffikri <yusuffiks99@gmail.com>
2023-02-13 18:33:33 +0800

    homepage update motion

--- src/sections/home/index.tsx
+++ src/sections/home/index.tsx
@@ -24,7 +24,9 @@ const index = ({setSelectedPage}: Props) => {
         {/* Main Header */}
         <div className='z-10 mt-32 md:basis-3/5'>
             {/* Heading */}
-            <motion.div className='md:-mt-20' initial='hidden' whileInView='visible' viewport={{once: true, amount: 0.5}}>
+            <motion.div className='md:-mt-20' initial='hidden' whileInView='visible' viewport={{once: true, amount: 0.5}} transition={{duration: 0.5}} variants={{ hidden: {opacity: 0, x:-50},
+             visible: {opacity: 1, x: 0},
+            }}>
                 <div className='relative'> 
                     <div className='before:absolute before:-top-20  before:-left-20 before:z-[-1] md:before:content-evolvetext'>
                         <img src={HomePageText} alt="homepage-text" />
