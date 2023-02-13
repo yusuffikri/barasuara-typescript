b4cc7449922df6d61cd7f4a27164403e3d8efa29
yusuffikri <yusuffiks99@gmail.com>
2023-02-13 18:02:54 +0800

    homepage update

--- src/sections/home/index.tsx
+++ src/sections/home/index.tsx
@@ -18,13 +18,13 @@ const index = ({setSelectedPage}: Props) => {
 
   return <section id="home" className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'>
     {/* Image and Main Header */}
-    <div>
+    <div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6  '>
         {/* Main Header */}
-        <div>
+        <div className='z-10 mt-32 md:basis-3/5'>
             {/* Heading */}
-            <div>
-                <div>
-                    <div>
+            <div className='md:-mt-20'>
+                <div className='relative'> 
+                    <div className='before:absolute before:-top-20  before:-left-20 before:z-[-1] md:before:content-evolvetext'>
                         <img src={HomePageText} alt="homepage-text" />
                     </div>
                 </div>
@@ -50,6 +50,18 @@ const index = ({setSelectedPage}: Props) => {
     </div>
 
     {/* Sponsor */}
+    {isAboveMediumScreens && (
+        <div>
+            <div>
+                <div>
+                    <img src={SponsorRedbull} alt="redbull" />
+                    <img src={SponsorForbes} alt="forbess" />
+                    <img src={SponsorFortune} alt="fortune" />
+                </div>
+            </div>
+        </div>
+
+    )}
   </section>
 }
 
