e565b5a8081d558ead791f3a43f1d17b1c0dd62e
yusuffikri <yusuffiks99@gmail.com>
2023-02-13 18:47:31 +0800

    homepage motion update

--- src/sections/home/index.tsx
+++ src/sections/home/index.tsx
@@ -20,11 +20,11 @@ const index = ({setSelectedPage}: Props) => {
 
   return <section id="home" className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'>
     {/* Image and Main Header */}
-    <div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6  '>
+    <motion.div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6 ' onViewportEnter={() => setSelectedPage(SelectedPage.Home)}>
         {/* Main Header */}
         <div className='z-10 mt-32 md:basis-3/5'>
             {/* Heading */}
-            <motion.div className='md:-mt-20' initial='hidden' whileInView='visible' viewport={{once: true, amount: 0.5}} transition={{duration: 0.5}} variants={{ hidden: {opacity: 0, x:-50},
+            <motion.div className='md:-mt-20' initial='hidden' whileInView='visible' viewport={{once: true, amount: 0.5}} transition={{delay: 0.2, duration: 1}} variants={{ hidden: {opacity: 0, x:-50},
              visible: {opacity: 1, x: 0},
             }}>
                 <div className='relative'> 
@@ -51,7 +51,7 @@ const index = ({setSelectedPage}: Props) => {
         <div className='flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end'>
             <img src={HomePageGraphic} alt="HomepageGraphic" />
         </div>
-    </div>
+    </motion.div>
 
     {/* Sponsor */}
     {isAboveMediumScreens && (
