ddf9f489d27457a00d3d080bc3738f65e635d23f
yusuffikri <yusuffiks99@gmail.com>
2023-02-15 08:51:50 +0800

    benefits page update

--- src/sections/benefits/Benefit.tsx
+++ src/sections/benefits/Benefit.tsx
@@ -17,6 +17,11 @@ const Benefit = ({icon, title, description, setSelectedPage}: Props) => {
                 {icon}
             </div>
         </div>
+        <h4 className="font-bold">{title}</h4>
+        <p className='my-3'>{description}</p>
+        <AnchorLink className='text-sm font-bold text-primary-500 underline hover:text-secondary-500' onClick={() => setSelectedPage(SelectedPage.Contacts)} href={`#${SelectedPage.Contacts}`}>
+         <p>Learn More</p>
+        </AnchorLink>
     </div>
     
   )
