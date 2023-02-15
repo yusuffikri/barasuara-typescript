21872f20e443b1122c4d72697f697f6d89117625
yusuffikri <yusuffiks99@gmail.com>
2023-02-15 15:43:35 +0800

    our classes update

---
+++ src/sections/classes/Class.tsx
@@ -0,0 +1,21 @@
+
+
+type Props = {
+    name: string;
+    description? : string;
+    image: string;
+}
+
+const Class = ({name, description = "some description", image}: Props) => {
+    const overlayStyles = `p-5 absolute z-30 flex h-[380px] w-[450px] flex-col items-center justify-center whitespace-normal bg-primary-500 text-center text-white opacity-0 transition duration-500 hover:opacity-90`;
+  
+    return <li className="relative mx-5 inline-block h-[380px] w-[450px]">
+        <div className={overlayStyles}>
+            <p>{name}</p>
+            <p>{description}</p>
+            <img src={image} alt={`${image}`} />
+        </div>
+    </li>
+}
+
+export default Class
