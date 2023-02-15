6f371d1378fdb478f778052541798313b8ef3d55
yusuffikri <yusuffiks99@gmail.com>
2023-02-15 15:27:39 +0800

    our classes update

--- src/sections/classes/index.tsx
+++ src/sections/classes/index.tsx
@@ -1,13 +1,45 @@
 import { SelectedPage } from '@/shared/types';
-import Image1  from '@/assets/image1.png';
-import Image2  from '@/assets/image2.png';
-import Image3  from '@/assets/image3.png';
-import Image4  from '@/assets/image4.png';
-import Image5  from '@/assets/image5.png';
-import Image6  from '@/assets/image6.png';
+import image1  from '@/assets/image1.png';
+import image2  from '@/assets/image2.png';
+import image3  from '@/assets/image3.png';
+import image4  from '@/assets/image4.png';
+import image5  from '@/assets/image5.png';
+import image6  from '@/assets/image6.png';
 import {motion } from 'framer-motion';
 import HText from '@/shared/HText';
 
+const ourclass: Array<ClassType> = [
+    {
+        name: "Weight Training Classes",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eaque ratione laborum a reprehenderit nam architecto doloremque molestiae. Fugit, odio aliquid accusamus iure modi quas.",
+        image: image1,
+    },
+    {
+        name: "Yoga Classes",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eaque ratione laborum a reprehenderit nam architecto doloremque molestiae. Fugit, odio aliquid accusamus iure modi quas.",
+        image: image2,
+    },
+    {
+        name: "Ab Core Classes",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eaque ratione laborum a reprehenderit nam architecto doloremque molestiae. Fugit, odio aliquid accusamus iure modi quas.",
+        image: image3,
+    },
+    {
+        name: "Adventure Classes",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eaque ratione laborum a reprehenderit nam architecto doloremque molestiae. Fugit, odio aliquid accusamus iure modi quas.",
+        image: image4,
+    },
+    {
+        name: "Fitness Classes",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eaque ratione laborum a reprehenderit nam architecto doloremque molestiae. Fugit, odio aliquid accusamus iure modi quas.",
+        image: image5,
+    },
+    {
+        name: "Training Classes",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eaque ratione laborum a reprehenderit nam architecto doloremque molestiae. Fugit, odio aliquid accusamus iure modi quas.",
+        image: image6,
+    },
+]
 
 
 type Props = {
@@ -21,12 +53,22 @@ const Classes = ({setSelectedPage}: Props) => {
         initial='hidden' whileInView='visible' viewport={{once: true, amount: 0.5}} transition={{delay: 0.2, duration: 0.5}} variants={{ hidden: {opacity: 0, x:-50},
         visible: {opacity: 1, x: 0},
        }}>
-        <div>
+        <div className='md:w-3/5'>
             <HText>
-                Our Classes
+                OUR CLASSES
             </HText>
+            <p className='py-5'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, quidem? Cum nisi atque modi asperiores quisquam voluptatibus quibusdam? Illo qui itaque amet porro laudantium, aperiam odio maiores ab nemo consequatur?
+            </p>
         </div>
         </motion.div>
+        <div className='mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden'>
+            <ul className='w-[2800px] whitespace-nowrap'>
+                {ourclass.map((item, index,) => (
+                    <Class/> 
+                ))}
+            </ul>
+        </div>
     </motion.div>
   </section>
 }
