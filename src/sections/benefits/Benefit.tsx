cacd809e307c44dd520a42664cfcc39bdafa392c
yusuffikri <yusuffiks99@gmail.com>
2023-02-15 09:09:28 +0800

    benefits page update

--- src/sections/benefits/Benefit.tsx
+++ src/sections/benefits/Benefit.tsx
@@ -1,6 +1,12 @@
 import { SelectedPage } from '@/shared/types';
+import { motion } from 'framer-motion';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
+const childVariant = {
+    hidden: { opacity: 0, scale: 0.9},
+    visible: {opacity: 1, scale: 1},
+    
+}
 
 type Props = {
     icon : JSX.Element;
@@ -11,7 +17,7 @@ type Props = {
 
 const Benefit = ({icon, title, description, setSelectedPage}: Props) => {
   return (
-    <div className='mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center '>
+    <motion.div variants={childVariant} className='mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center '>
         <div className='mb-4 flex justify-center'>
             <div className='rounded-full border-2 border-gray-100 bg-primary-100 p-4'>
                 {icon}
@@ -22,7 +28,7 @@ const Benefit = ({icon, title, description, setSelectedPage}: Props) => {
         <AnchorLink className='text-sm font-bold text-primary-500 underline hover:text-secondary-500' onClick={() => setSelectedPage(SelectedPage.Contacts)} href={`#${SelectedPage.Contacts}`}>
          <p>Learn More</p>
         </AnchorLink>
-    </div>
+    </motion.div>
     
   )
 }
