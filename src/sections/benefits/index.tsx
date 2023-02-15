212e0bf68be99b09cbb7d00c59718f4e2211cc24
yusuffikri <yusuffiks99@gmail.com>
2023-02-15 11:32:48 +0800

    benefits page update

--- src/sections/benefits/index.tsx
+++ src/sections/benefits/index.tsx
@@ -38,7 +38,10 @@ const Benefits = ({setSelectedPage}: Props) => {
   return <section id="benefits" className="mx-auto min-h-full w-5/6 py-20 ">
     <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}>
        {/* Header */}
-        <motion.div className="md:my-5 md:w-3/5">
+        <motion.div className="md:my-5 md:w-3/5"
+        initial='hidden' whileInView='visible' viewport={{once: true, amount: 0.5}} transition={{delay: 0.2, duration: 0.5}} variants={{ hidden: {opacity: 0, x:-50},
+        visible: {opacity: 1, x: 0},
+       }}>
             <HText>MORE THAN JUST A GYM.</HText>
             <p className="my-5 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquam tempora corrupti quaerat dolor cumque voluptatibus sed, molestiae similique id fugit libero hic magni aliquid nesciunt ex alias debitis corporis.</p> 
         </motion.div>
@@ -49,6 +52,32 @@ const Benefits = ({setSelectedPage}: Props) => {
               {benefits.map((benefit:BenefitType) => (<Benefit key={benefit.title} icon={benefit.icon} title={benefit.title} description={benefit.description} setSelectedPage={setSelectedPage}/>))}
             </motion.div>
           
+       {/* Graphic And Description */}
+       <div>
+        {/* Graphic */}
+        <img src="" alt="" />
+
+        {/* Description */}
+        <div>
+          {/* Title */}
+        <div className="relative">
+          <div className="before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractwaves">
+            <div>
+              <HText>
+                MILLIONS OF HAPPY MEMBER GETTING {""} 
+                <span className="text-primary-500">FIT</span>
+              </HText>
+            </div>
+
+          </div>
+        </div>
+          {/* Descript */}
+
+          {/* Button */}
+
+        </div>
+       </div>
+
     </motion.div> 
   </section>
 }
