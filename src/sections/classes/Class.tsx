f5f51346db05d5c773dca4d5f889147bd596dfbc
yusuffikri <yusuffiks99@gmail.com>
2023-02-19 10:13:05 +0800

    update class hover

--- src/sections/classes/Class.tsx
+++ src/sections/classes/Class.tsx
@@ -1,19 +1,17 @@
-
-
 type Props = {
     name: string;
-    description? : string;
+    description?: string;
     image: string;
 }
 
-const Class = ({name, description = "some description", image}: Props) => {
-    const overlayStyles = `p-5 absolute z-30 flex h-[380px] w-[450px] flex-col items-center justify-center whitespace-normal bg-primary-500 text-center text-white opacity-0 transition duration-500 hover:opacity-90`;
-  
+const Class = ({ name, description = "some description", image }: Props) => {
+    const overlayStyles = `p-5 absolute z-30 flex h-[380px] w-[450px] flex-col items-center justify-center whitespace-normal bg-primary-500 text-center text-white group`;
+
     return <li className="relative mx-5 inline-block h-[380px] w-[450px]">
         <div className={overlayStyles}>
-            <p>{name}</p>
-            <p>{description}</p>
-            <img src={image} alt={`${image}`} />
+            <p className="opacity-0 transition duration-700 group-hover:opacity-90 ">{name}</p>
+            <p className="opacity-0 transition duration-900 group-hover:opacity-90 ">{description}</p>
+            <img className="" src={image} alt={`${image}`} />
         </div>
     </li>
 }
