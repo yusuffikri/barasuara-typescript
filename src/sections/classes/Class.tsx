c518694635f8c1c52af8ec9c83a878de0a1b3481
yusuffikri <yusuffiks99@gmail.com>
2023-02-19 10:14:34 +0800

    update class hover

--- src/sections/classes/Class.tsx
+++ src/sections/classes/Class.tsx
@@ -5,15 +5,13 @@ type Props = {
 }
 
 const Class = ({ name, description = "some description", image }: Props) => {
-    const overlayStyles = `p-5 absolute z-30 flex h-[380px] w-[450px] flex-col items-center justify-center whitespace-normal bg-primary-500 text-center text-white group`;
+    const overlayStyles = `p-5 absolute top-0 z-30 flex h-[380px] w-[450px] flex-col items-center justify-center whitespace-normal bg-primary-500 bg-opacity-0 text-center text-white group-hover:bg-opacity-60`;
 
-    return <li className="relative mx-5 inline-block h-[380px] w-[450px]">
+    return <li className="group relative mx-5 inline-block h-[380px] w-[450px]">
+        <img className="" src={image} alt={`${image}`} />
         <div className={overlayStyles}>
-            <p className="opacity-0 transition duration-700 group-hover:opacity-90 ">{name}</p>
-            <p className="opacity-0 transition duration-900 group-hover:opacity-90 ">{description}</p>
-            <img className="" src={image} alt={`${image}`} />
+            <p className="opacity-0 transition mb-5 font-bold duration-900  group-hover:opacity-100">{name}</p>
+            <p className="opacity-0 transition duration-900 group-hover:opacity-100 ">{description}</p>
         </div>
     </li>
 }
-
-export default Class
