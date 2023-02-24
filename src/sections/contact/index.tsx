a3fb3fb512358451c1dc463bb9f15f169efb8fec
yusuffikri <yusuffiks99@gmail.com>
2023-02-24 23:57:56 +0800

    contact page update

--- src/sections/contact/index.tsx
+++ src/sections/contact/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import { SelectedPage } from "@/shared/types"
+import { motion } from "framer-motion";
 
-type Props = {}
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void;
+}
 
 const Contact
- = (props: Props) => {
+ = (setSelectedPage: Props) => {
   return (
     <div>Contact
-        
     </div>
   )
 }
