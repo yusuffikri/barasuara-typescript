4e3ec0796a6db28ca34c8f3267a4143275787fb3
yusuffikri <yusuffiks99@gmail.com>
2023-02-25 15:06:43 +0800

    contact page update

--- src/sections/contacts/index.tsx
+++ src/sections/contacts/index.tsx
@@ -25,6 +25,15 @@ const Contacts
 
         </motion.div>
 
+        {/* FORM AND IMAGE */}
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div className="mt-10 basis-3/5 md:mt-0" initial='hidden' whileInView='visible' viewport={{once: true, amount: 0.5}} transition={{delay: 0.2, duration: 0.5}} variants={{ hidden: {opacity: 0, y:-50},
+        visible: {opacity: 1, y: 0},
+       }}>
+
+          </motion.div>
+        </div>
+
     </motion.div>
   </section>
 }
