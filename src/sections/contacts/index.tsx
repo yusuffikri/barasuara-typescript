b2c9a386fb731db668ba0cb59b507e7605f67376
yusuffikri <yusuffiks99@gmail.com>
2023-02-25 14:18:14 +0800

    contact page update

--- src/sections/contacts/index.tsx
+++ src/sections/contacts/index.tsx
@@ -1,7 +1,7 @@
 import { SelectedPage } from "@/shared/types"
 import { motion } from "framer-motion";
 import { useForm } from "react-hook-form";
-import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic"
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png"
 
 
 type Props = {
@@ -10,10 +10,12 @@ type Props = {
 
 const Contacts
  = ({setSelectedPage}: Props) => {
-  return (
-    <div>Contacts
-    </div>
-  )
+  return <section id="contacts" className="mx-auto w-5/6 pt-24 pb-32">
+    <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Contacts
+      )}>
+
+    </motion.div>
+  </section>
 }
 
 export default Contacts
